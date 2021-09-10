3b4ee4e61a29606222653674bf28515bec7b365a
KamilMieniek <kamil.mieniek@gmail.com>
2021-09-10 22:36:38 +0200

    add user profile, show users public stories

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const session = require('express-session');
 const connectDB = require('./config/db');
 const exphbs = require('express-handlebars');
@@ -20,16 +21,37 @@ const app = express();
 //Body Parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      var method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 
 //Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.set('view engine', 'hbs');
+//Handlebars helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require('./helpers/hbs');
+
 //Handlebars
 app.engine(
   '.hbs',
   exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
     defaultLayout: 'main',
     extname: '.hbs',
   })
@@ -48,6 +70,12 @@ app.use(
 //Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+//Set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
